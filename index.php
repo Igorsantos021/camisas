bed240c91265f5b8e9c6e42d6f42add262b17323
Igor Santos <santosigor868@gmail.com>
2024-03-26 22:22:27 -0300

    Up Run

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Camisas</title>
+</head>
+<body style="background-color: #e2eaef;">
+
+    <div class="container p-4 d-flex flex-nowrap grid gap-0 column-gap-5">
+        <i class="bi bi-heart"></i>
+        <div class="row gx-5">
+            <div class="container text-center">
+                <div class="row">
+                    <div class="container text-center">
+                        <div class="row ">
+                            <div class="col">
+                                <div class="card shadow-sm bi bi-heart" style="width: 15rem;">
+                                    <img src="foto1.png" class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            
+                                            <p class="card-text">Camisa Polo Masculina</p>
+                                            <h5>Roupas</h5>
+                                            <strike>R$ 49,99</strike>  <span class="text-success">R$39,99</span>
+                                            <button type="button" class="btn btn-success btn-sm">Adicionar ao carrinho</button>
+                                        </div>
+                                </div>
+                            </div>
+   
+    
+                        </div>
+                    </div>                                        
+               </div> 
+           </div>
+      </div>
+
+      <div class="row gx-4">
+            <div class="container text-center">
+                <div class="row">
+                    <div class="container text-center">
+                        <div class="row align-items-start">
+                            <div class="col">
+                                <div class="card shadow-sm" style="width: 15rem;">
+                                    <img src="foto1.png" class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            <p class="card-text">Camisa Polo Masculina</p>
+                                            <h5>Roupas</h5>
+                                            <strike>R$ 49,99</strike>  <span class="text-success">R$39,99</span>
+                                            <button type="button" class="btn btn-success btn-sm">Adicionar ao carrinho</button>
+                                        </div>
+                                </div>
+                            </div>
+   
+    
+                        </div>
+                    </div>                                        
+               </div> 
+           </div>
+      </div>
+
+      <div class="row gx-4">
+            <div class="container text-center">
+                <div class="row">
+                    <div class="container text-center">
+                        <div class="row align-items-start">
+                            <div class="col">
+                                <div class="card shadow-sm" style="width: 15rem;">
+                                    <img src="foto1.png" class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            <p class="card-text">Camisa Polo Masculina</p>
+                                            <h5>Roupas</h5>
+                                            <strike>R$ 49,99</strike>  <span class="text-success">R$39,99</span>
+                                            <button type="button" class="btn btn-success btn-sm">Adicionar ao carrinho</button>
+                                        </div>
+                                </div>
+                            </div>
+   
+    
+                        </div>
+                    </div>                                        
+               </div> 
+           </div>
+      </div>
+
+      <div class="row gx-4">
+            <div class="container text-center">
+                <div class="row">
+                    <div class="container text-center">
+                        <div class="row align-items-start">
+                            <div class="col">
+                                <div class="card shadow-sm" style="width: 15rem;">
+                                    <img src="foto1.png" class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            <p class="card-text">Camisa Polo Masculina</p>
+                                            <h5>Roupas</h5>
+                                            <strike>R$ 49,99</strike>  <span class="text-success">R$39,99</span>
+                                            <button type="button" class="btn btn-success btn-sm">Adicionar ao carrinho</button>
+                                        </div>
+                                </div>
+                            </div>
+   
+    
+                        </div>
+                    </div>                                        
+               </div> 
+           </div>
+      </div>
+
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index01.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <title>Document</title>
+</head>
+<body>
+    <body style="background-color: #e2eaef;">
+        <div class="row gx-5">
+            <div class="row gx-5">
+                <div><!-- coluna -->
+                    <div> <!-- ícone -->
+                        <i class="bi bi-heart"></i>
+                    </div>
+                    <img src="protudos/fotos1.png" alt="Camisa Polo Masculina" cl>
+                    <h4>Camisa Polo Masculina</h4>
+                    <strong>Roupas</strong>
+                    <div> <!-- linha de preços --></div>
+                        <div> <!-- coluna preço antigo -->
+                            <s>R$ 49.99</s>
+                        </div>
+                        <div> <!-- coluna preço novo -->
+                            <strong>R$ 39,99</strong>
+                        </div>
+                </div> <!-- fechando linhade preço -->
+                <a href="#">Adicionar ao carrinho </a>
+            </div> <!-- fechamento produto -->
+        </div> <!-- fechamento coluna -->
+       
+    </body>
+</body>
+</html>
